029604518f536ebedf9ce8d55937e2b229fcb40c
freyjaprescott <155899136+freyjaprescott@users.noreply.github.com>
2024-10-20 10:28:33 -0400

    preheat pizza rolls

--- UART/transmit/transmit.ino
+++ UART/transmit/transmit.ino
@@ -2,7 +2,15 @@
 SoftwareSerial softSerial(8, 9);  // RX, TX 
 void setup()  {
   softSerial.begin(9600);
+  Serial.begin(9600);
 } 
 void loop()  { 
-  softSerial.write("W"); 
+  if(Serial.available()){
+    int outByte = Serial.read();
+    softSerial.write(outByte);
+  }
+  if(softSerial.available()){
+    int inByte = softSerial.read();
+    Serial.write(inByte);
+  }
 }
