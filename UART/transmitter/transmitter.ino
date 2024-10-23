e0a37ae8425b5f958889b7a63a081b5a2090cc37
Nick Anastasoff <anastasoffnick@gmail.com>
2024-10-22 22:05:26 -0400

    added constnats

--- UART/transmitter/transmitter.ino
+++ UART/transmitter/transmitter.ino
@@ -11,18 +11,22 @@
 #include "DigitalConditionSensor.h"
 #include <SparkFun_RV8803.h>
 
-SoftwareSerial softSerial(8, 9);  // RX, TX 
-
+const int rx_pin = 8;
+const int tx_pin = 9;
+const int dht_pin = 6;
 const int batteryPin = A0;  // BAT pin connected to A0
+const int flood_pin = 7;
+
 const float referenceVoltage = 5.0; // If using a 5V Arduino, adjust if necessary
 const float maxADC = 1023.0;  // Maximum value for a 10-bit ADC
 
+SoftwareSerial softSerial(rx_pin, tx_pin);  // RX, TX 
 
 Ezo_board PH = Ezo_board(99, "PH");       // pH, address is 99 and name is "PH"
 Ezo_board EC = Ezo_board(100, "EC");      // Conductivity, address is 100 and name is "EC"
 Ezo_board TM = Ezo_board(102, "TM");      // water temp, address is 102 and name is "TM"
-DigitalConditionSensor floodSensor("FloodSensor", 7); //create a flood detection sensor object on pin 9
-DHT dht(6, DHT22);   //create a dht circut object on pin 8
+DigitalConditionSensor floodSensor("FloodSensor", flood_pin); //create a flood detection sensor object on pin 9
+DHT dht(dht_pin, DHT22);   //create a dht circut object on pin 8
 
 RV8803 rtc; // Create an instance of the RV8803 RTC
 void step1();
